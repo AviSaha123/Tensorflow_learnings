b3c19c4ff2b48123c5efe9d6d0b3dc77c706bb79
AviSaha123 <avirupsaha357@gmail.com>
2022-10-05 17:17:02 +0530

    making a helper.py to store all functions

---
+++ helper.py
@@ -0,0 +1,38 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+def plot_decision_boundary(model, X, y):
+    """Plots the decision boundary created by a model  predicting on X
+
+    Args:
+        model (_type_): model
+        X (_type_): Training data
+        y (_type_): Testing Data
+    """
+    # Define the axis boundaries of the plot and create meshgrid
+    x_min, x_max = X[:,0].min() - 0.1 , X[:,0].max()+0.1
+    y_min, y_max = X[:,1].min() - 0.1 , X[:,1].max()+0.1
+    xx,yy = np.meshgrid(np.linspace(x_min,x_max,100),
+                        np.linspace(y_min,y_max,100))
+    
+    # Create X values to predict
+    
+    x_in = np.c_[xx.ravel(),yy.ravel()]
+    
+    # Make Predictions
+    y_pred = model.predict(x_in)
+    
+    # Check for multi class classification
+    
+    if len(y_pred[0]) > 1:
+        print('Doing multicalss classification')
+        y_pred = np.argmax(y_pred, axis = 1).reshape(xx.shape)
+    else:
+        print('Binary Classification')
+        y_pred = np.round(y_pred).reshape(xx.shape)
+
+    # Plot the decision boundary
+    plt.contourf(xx,yy,y_pred,cmap = plt.cm.RdYlBu, alpha = 0.7)
+    plt.scatter(X[:,0], X[:,1], c =y ,s =40, cmap = plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
