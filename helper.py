a5a9c665c449352739dd13c464aac95c0a34cee4
AviSaha123 <avirupsaha357@gmail.com>
2022-10-06 23:02:35 +0530

    change in figure size

--- helper.py
+++ helper.py
@@ -34,7 +34,7 @@ def plot_decision_boundary(model, X, y):
             y_pred = np.round(y_pred).reshape(xx.shape)
 
         # Plot the decision boundary
-        plt.figure(figsize=(10,6))
+        # plt.figure(figsize=(10,6))
         plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
         plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
         plt.xlim(xx.min(), xx.max())
