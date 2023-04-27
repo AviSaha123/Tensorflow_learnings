611784cf6e7cb07dfa727470d30548aaf0bea47b
avi4peeps <avi4peeps@gmail.com>
2023-04-27 17:10:07 +0530

    update with unzip function

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import zipfile
 import itertools
 import random
 import numpy as np
@@ -279,3 +281,57 @@ def plot_loss_curves(history):
   plt.title('Accuracy')
   plt.xlabel('Epochs')
   plt.legend()
+
+
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
+
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+
+
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+
+  Args:
+    dir_path (str): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+
+# Function to evaluate: accuracy, precision, recall, f1-score
+
+
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+      y_true: true labels in the form of a 1D array
+      y_pred: predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(
+      y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                   "precision": model_precision,
+                   "recall": model_recall,
+                   "f1": model_f1}
+  return model_results
